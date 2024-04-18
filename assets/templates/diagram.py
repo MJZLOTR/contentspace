f9a59556fef6c3ecb629d8e85317333f56ac30b9
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2024-04-18 11:22:35 +0000

    added graphviz example in templates

--- assets/templates/diagram.py
+++ assets/templates/diagram.py
@@ -1,11 +1,25 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams import Diagram, Cluster
+from diagrams.opentelekomcloud.computing import Ecs
+from diagrams.opentelekomcloud.database import Rds
+from diagrams.opentelekomcloud.network import Elb, Dns, Eip
 
-with Diagram("Grouped Workers", show=False, direction="TB"):
-    ELB("lb") >> [EC2("worker1"),
-                  EC2("worker2"),
-                  EC2("worker3"),
-                  EC2("worker4"),
-                  EC2("worker5")] >> RDS("events")
+with Diagram("Basic Web App", show=False):
+    dns = Dns("dns")
+    eip = Eip("eip")
+
+    dns >> eip
+
+    with Cluster("vpc"):
+        elb = Elb("dns")
+
+        with Cluster("app-subnet"):
+            app = [Ecs("app1"),
+            Ecs("app2")]
+
+        with Cluster("db-subnet"):
+            db_primary = Rds("primary")
+            db_primary - [Rds("replica1"),
+                        Rds("replica2")]
+
+    eip >> elb >> app >> db_primary
+    
